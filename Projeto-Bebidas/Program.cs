4ed6694512f377ec3a7ead3ab622094f0b877345
Mateus Moura Ferreira <48527217856@info.local>
2023-04-04 09:07:31 -0300

    alteracoes do dia 04-04

---
+++ Projeto-Bebidas/Program.cs
@@ -0,0 +1,69 @@
+﻿Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine($@"
+
+--------------------------------------
+|Escolha a sua bebida no nosso menu! |
+|Digite '1' para Coca-Cola           |
+|Digite '2' para Pina Colada         |
+|Digite '3' para um copo Jack Daniels|
+|Digite '4' para Sprite              |
+--------------------------------------
+");
+
+Console.ResetColor();
+
+Console.WriteLine($"Escolha sua bebida");
+char bebida = char.Parse(Console.ReadLine());
+
+Console.WriteLine($"Voce deseja acrescentar gelo? responda com sim ou nao.");
+string gelo = Console.ReadLine();
+
+string sabor = "bebida nao selecionada!";
+
+switch (bebida)
+{
+    case '1':
+    sabor = "Coca-Cola";
+    break;
+
+    case '2':
+    sabor = "Pina Colada";
+    break;
+
+    case '3':
+    sabor = "Jack Daniels";
+    break;
+
+    case '4':
+    sabor = "Sprite";
+    break;
+
+    
+    default:
+    Console.WriteLine($"Bebida nao selecionada. ");
+    break;
+
+    }
+
+switch (gelo)
+{
+case "sim":
+gelo = "com gelo! Boa bebida. ";
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($" Voce selecionou a bebida {sabor} {gelo}");
+Console.ResetColor();
+break;
+
+case "nao":
+gelo = "sem gelo! Boa bebida. ";
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($" Voce selecionou a bebida {sabor} {gelo}");
+Console.ResetColor();
+break;
+
+default:
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"Gelo nao selecionado.");
+Console.ResetColor();
+break;
+}
---
+++ Projeto-Calculadora/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine($@"
+'+' para somar
+'-' para subtrair
+'*' para multiplicar
+'/' para dividir
+");
+
+Console.WriteLine($"Digite sua operacao");
+char operacao = char.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o numero 1");
+float numero1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o numero 2");
+float numero2 = float.Parse(Console.ReadLine());
+
+float resultado = 0;
+
+switch (operacao)
+{
+    case '+' :
+    resultado = (numero1+numero2);
+    Console.WriteLine($"o Resultado da sua soma foi {resultado}");
+    break;
+
+    case '-' :
+    resultado = (numero1-numero2);
+    Console.WriteLine($"O Resultado da sua subtracao foi {resultado}");
+    break;
+
+    case '*':
+    resultado = (numero1*numero2);
+    Console.WriteLine($"O Resultado da sua multiplicacao foi {resultado}");
+    break;
+
+    case '/':
+    resultado = (numero1/numero2);
+    Console.WriteLine($"O Resultado da sua divisao foi {resultado}");
+    break;
+
+    default:
+        Console.WriteLine($"Erro na operacao");
+    break;
+}
