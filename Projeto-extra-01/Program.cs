2bb7a93654aeaf7d440401f69d8420617165887a
Mateus Moura Ferreira <48527217856@info.local>
2023-04-06 10:59:03 -0300

    alteracoes do dia 06-04

---
+++ Projeto-extra-01/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+for (string i=1; i <=10; i++)
+{
+Console.WriteLine($"Seu sexo e M ? ");
+string sexo = Console.ReadLine();
+
+Console.WriteLine($"Gostaram do produto? ");
+string produto = Console.ReadLine();
+    
+}
+
+
---
+++ Projeto-pericles/Program.cs
@@ -0,0 +1,11 @@
+﻿bool idadeCerta = false;
+
+do
+{
+    Console.WriteLine($"Qual a idade do Pericles: ");
+    int idade = int.Parse(Console.ReadLine());
+
+    if ( idade == 53)
+    idadeCerta = true;
+    
+} while(!idadeCerta);
